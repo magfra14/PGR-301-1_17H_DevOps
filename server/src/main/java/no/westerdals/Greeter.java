9e3b39fbbb6f4fa0f5da11e0bc0cb91bd39b0f9c
magfra14 <magfra14@student.westerdals.no>
2017-12-04 14:46:36 +0100

    added app

---
+++ server/src/main/java/no/westerdals/Greeter.java
@@ -0,0 +1,13 @@
+package no.westerdals;
+
+
+public class Greeter {
+
+    public Greeter() {
+
+    }
+
+    public String greet(String someone) {
+        return String.format("Hello, %s!", someone);
+    }
+}
